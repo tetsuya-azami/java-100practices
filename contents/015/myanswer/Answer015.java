88329257a33baac0799a5ecc80999efdaf892d0f
tetuya-azami <zqianjian35@gmail.com>
2023-12-20 08:13:02 +0900

    answer no015

---
+++ contents/015/myanswer/Answer015.java
@@ -0,0 +1,13 @@
+package contents.015.myanswer;
+
+public class Answer015 {
+	private final String name;
+
+	public Answer015() {
+		this.name = "no name";
+	}
+
+	public Answer015(String name) {
+		this.name = name;
+	}
+}
